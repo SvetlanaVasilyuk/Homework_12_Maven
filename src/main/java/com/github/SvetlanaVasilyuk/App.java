acb4ae1158a58ccbc84faf6ba1e65e962cec3f49
Svetlana Vasilyuk <lanarusufa@gmail.com>
2020-03-14 22:35:47 +0300

    Добавлена задача по занятию 12.

--- src/main/java/com/github/SvetlanaVasilyuk/App.java
+++ src/main/java/com/github/SvetlanaVasilyuk/App.java
@@ -1,10 +1,21 @@
 package com.github.SvetlanaVasilyuk;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
-public class App 
+public class App
 {
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void putToLog(){
+        LOG.error("Сообщение об ошибке");
+        LOG.warn("Сообщение с предупреждением");
+        LOG.info("Информационное сообщение");
+        LOG.trace("Сообщение-трассировка");
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        putToLog();
     }
 }
